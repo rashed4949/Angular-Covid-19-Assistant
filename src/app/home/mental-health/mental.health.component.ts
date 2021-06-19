fdbeab63067e512af27b5d3477cc7f27f0dce734
Md Rashedul Islam <rrashed4@gmail.com>
2021-06-19 15:04:08 +0200

    mental health

---
+++ src/app/home/mental-health/mental.health.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'mental-health',
+    templateUrl: './mental.health.component.html',
+    styleUrls: ['./mental.health.component.css']
+})
+export class MentalHealthComponent implements OnInit {
+
+    constructor(
+
+    ) {
+    }
+
+
+    ngOnInit(): void {
+
+    }
+}
